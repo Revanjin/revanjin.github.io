5b8e1b1464066e9e002e02d9b9dfff27c643ddae
revanjin <alex.tanachai@hotmail.de>
2018-03-28 10:22:13 +0200

    test

--- components/home.js
+++ components/home.js
@@ -1,30 +1,3 @@
 angular.module('app').component('home', {
-    template:  '<div class="container">\n' +
-    '    <div class="flex-container clearfix">\n' +
-    '    <div class="text-center p-5 rounded">\n' +
-    '    <h2 class="section-heading mb-4">\n' +
-    '    <span>Why am I doing this?!</span>\n' +
-    '    </h2>\n' +
-    '    <img src="https://cdn-images-1.medium.com/max/545/0*Le4NWnMPwFOvu5ea.png" style="float:left;">\n' +
-    '    <p class="mt-6">I am trying to improve my programming skills like Javascript, Angular.js, Node.js and Typescript\n' +
-    'nevertheless sometimes it feels painful to find a good start.\n' +
-    '</p>\n' +
-    '<p class="mt-4">\n' +
-    '    Perchance Github Pages could be a good step on\n' +
-    'working with these Programming Languages.\n' +
-    '</p>\n' +
-    '<p class="mt-4"> Everyone who worked with a Framework, especially AngularJS might know these problems :)\n' +
-    '</p>\n' +
-    '</div>\n' +
-    '</div>\n' +
-    '<div class="flex-container container clearfix mb-5">\n' +
-    '    <div class="text-center flex-three"><p>I am just a random text in a Flexbox. I am on the left side and centered!\n' +
-    '</p></div>\n' +
-    '<div class="text-center flex-three"><p>I am just a random text in a Flexbox. I am in the middle side and centered!\n' +
-    '</p></div>\n' +
-    '<div class="text-center flex-three"><p>I am just a random text in a Flexbox. I am on the right side and centered!\n' +
-    '</p></div>\n' +
-    '</div>\n' +
-    '\n' +
-    '</div>'
+    template: require('./home.html')
 })
