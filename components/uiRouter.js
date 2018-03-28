b021add8612dd253f88a6edfb5e059005e75965c
revanjin <alex.tanachai@hotmail.de>
2018-03-28 09:43:09 +0200

    test

--- components/uiRouter.js
+++ components/uiRouter.js
@@ -1,8 +1,8 @@
 angular.module('app').component('uiRouter', {
     template:
         '<div class="flex-container">\n' +
-        '        <div class="flex-box-center hvr-bubble-float-right"><p>Ich bin in der roten Box, mittig. Bin ich zu lang, dann werde ich gewrapped</p></div>\n' +
-        '        <div class="flex-box-right hvr-grow-rotate"><p>Und ich bin rechts, in einer grünen Box</p></div>\n' +
-        '        <div class="flex-box-left hvr-grow"><img src="../img/dewritos.jpg"></div>\n' +
+        '        <div class="flex-box-center red hvr-bubble-float-right"><p>Ich bin in der roten Box, mittig. Bin ich zu lang, dann werde ich gewrapped</p></div>\n' +
+        '        <div class="flex-box-right blue hvr-grow-rotate"><p>Und ich bin rechts, in einer blauen Box</p></div>\n' +
+        '        <div class="flex-box-left green hvr-grow"><img src="../img/dewritos.jpg"></div>\n' +
         '</div>'
 });
