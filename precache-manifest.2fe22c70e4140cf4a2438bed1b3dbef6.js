0314fcdc605e531cf59cfc91061ae8c21a71d1ef
Revanjin <Revanjin@users.noreply.github.com>
2020-05-31 20:09:20 +0200

    updating portfolio
    added full portfolio built with ReactJS, Styles via Scss and Gulp, added IE EasterEgg

---
+++ precache-manifest.2fe22c70e4140cf4a2438bed1b3dbef6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a862a4452eded5342e062770b4469d86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28ddf239ec4363c22811",
+    "url": "./static/css/main.a591dd3b.chunk.css"
+  },
+  {
+    "revision": "900ebc5a4307d5df0c33",
+    "url": "./static/js/2.efdc95d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.efdc95d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ddf239ec4363c22811",
+    "url": "./static/js/main.3e6c5dcf.chunk.js"
+  },
+  {
+    "revision": "ce14f66c8894118ce375",
+    "url": "./static/js/runtime-main.f92aead6.js"
+  },
+  {
+    "revision": "afc155f89dc18ad04394ab7928bfa8e3",
+    "url": "./static/media/dog.afc155f8.png"
+  },
+  {
+    "revision": "1afe2697cc199dfc8b397a02e167339d",
+    "url": "./static/media/info.1afe2697.png"
+  },
+  {
+    "revision": "4083f386a4eda0cb488f4dc525f82b8d",
+    "url": "./static/media/rocket.4083f386.png"
+  }
+]);
