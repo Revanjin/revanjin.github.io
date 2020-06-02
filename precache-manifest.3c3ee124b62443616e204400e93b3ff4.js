dc49a5740a89fcc0c086bceaa6550164a27d2632
Revanjin <Revanjin@users.noreply.github.com>
2020-06-02 08:58:22 +0200

    updating text
    i run npm run build on the react app to create a prod build which i upload

---
+++ precache-manifest.3c3ee124b62443616e204400e93b3ff4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bb6c4ecebe7e549ef334900cbab95f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f315abff852f583553f3",
+    "url": "./static/css/main.f6643640.chunk.css"
+  },
+  {
+    "revision": "900ebc5a4307d5df0c33",
+    "url": "./static/js/2.efdc95d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.efdc95d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f315abff852f583553f3",
+    "url": "./static/js/main.faa88006.chunk.js"
+  },
+  {
+    "revision": "ce14f66c8894118ce375",
+    "url": "./static/js/runtime-main.f92aead6.js"
+  },
+  {
+    "revision": "afc155f89dc18ad04394ab7928bfa8e3",
+    "url": "./static/media/dog.afc155f8.png"
+  },
+  {
+    "revision": "1afe2697cc199dfc8b397a02e167339d",
+    "url": "./static/media/info.1afe2697.png"
+  },
+  {
+    "revision": "4083f386a4eda0cb488f4dc525f82b8d",
+    "url": "./static/media/rocket.4083f386.png"
+  }
+]);
